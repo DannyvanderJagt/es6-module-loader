5a62ca57f04e5417e4e51f49aba83dd74f9ae58f
Danny van der Jagt <dannyvanderjagt@gmail.com>
2015-09-06 15:43:24 +0200

    [Docs] Added in code documentation.

--- README.md
+++ README.md
@@ -6,11 +6,11 @@ Import and use es6/es2015 modules directly without the need to pre-compile them.
 
 ## Todo
 **Code:**
-- [ ] Change the base paths 2 directories back.
-- [ ] Change the index require path to a relative path. It is now an absolute path.
+- [x] Change the base paths 2 directories back.
+- [x] Change the index require path to a relative path. It is now an absolute path.
 
 **Documentation:**
-- [ ] Add code documentation.
+- [x] Add code documentation.
 - [ ] Write a usage + tutorial.
 - [ ] Medium post ?
 - [ ] Write docs.
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import Fs from 'fs';
 import Path from 'path';
 
+/**
+ * Module Loader.
+ * @namespace moduleloader
+ */
 let ModuleLoader = {
+    /**
+     * @var {Object} paths - The base paths.
+     */
     paths:{
         base: Path.join(__dirname, '../'),
         packageFile: Path.join(__dirname,'../','package.json'),
@@ -9,93 +16,66 @@ let ModuleLoader = {
         es6Modules: Path.join(__dirname,'../','es6_modules')
     },
     
+    /**
+     * Prepare and run everything.
+     * @name initialize
+     * @function
+     */
     initialize(){
         this.packageFile = this.getPackage(this.paths.base, true);
         
         this.mentionedInPackageFile = this.getAllFromPackageFile(this.packageFile);
         
         // Collect all the names of es6 packages.
-        this.esPackages = this.getAllPackages();
-    },
-    
-    getAllPackages(){
-        let _packages = this.getAllFromNodeModules();
-
-        console.log('PACKAGES:', _packages);
+        this.esPackages = this.getAllPackages(this.mentionedInPackageFile);
+        
         // Move all the packages into es6_modules.
-        this.movePackages(_packages);
+        this.movePackages(this.esPackages);
         
         // Create the redirect file.
-        this.createRedirectForPackages(_packages);
+        this.createRedirectForPackages(this.esPackages);
     },
     
-    createRedirectForPackages(packages){
-        console.log(packages);
-        packages.forEach((_packageObject) => {
-            console.log(_packageObject);
-            let _package = Object.keys(_packageObject)[0];
-            let _mainPath = _packageObject[_package];
-            let _oldPath = Path.join(this.paths.nodeModules, _package);
-            let _currentPath = Path.join(this.paths.es6Modules, _package);
-            
-            let _indexFile = this.generateIndexFile(_currentPath, _mainPath);
-            let _packageFile = this.generatePackageFile(_package);
-            
-            // Create the directory.
-            Fs.mkdirSync(_oldPath);
-            
-            // Create the package.json file.
-            Fs.writeFileSync(_oldPath + '/package.json', _packageFile);
-            
-            // Create the redirect file.
-            console.log('index', _indexFile);
-            Fs.writeFileSync(_oldPath + '/index.js', _indexFile);
-        });
-    },
-    
-    generateIndexFile(path, mainPath){
-        let _path = Path.join(path, mainPath);
-        let _file = "module.exports = require('"+_path+"');";//"import Module from '"+_path+"'; export default module;";
-        return _file;
-    },
-    
-    generatePackageFile(pkg){
-        let _object = {
-            name: pkg,
-            main: 'index.js',
-            es6Replaced: true,
-        };
-        return JSON.stringify(_object);
-    },
-    
-    
-    movePackages(packages){
-        if(!Fs.existsSync(this.paths.es6Modules)){
-            Fs.mkdir(this.paths.es6Modules);
-        }
+    /**
+     * Get the package.json file.
+     * @param  {String} path        - The path to the parent directory of the package.json file.
+     * @param  {Boolean} throwError - Throw an error when the package.json file can't be found.
+     * @return {Object} The package.json contents.
+     */
+    getPackage(path, throwError = false){
+        let file, json;
         
-        packages.forEach((_packageObject) => {
-            let _package = Object.keys(_packageObject)[0];
-            let _currentPath = Path.join(this.paths.nodeModules, _package);
-            let _newPath = Path.join(this.paths.es6Modules, _package);
-            
-            if(!Fs.existsSync(_currentPath)){
-                return false; // The module is already moved.
+        path = Path.join(path, 'package.json');
+        
+        // Get the package.json file.
+        try{
+            file = Fs.readFileSync(path, 'utf-8');
+        }catch(error){
+            if(throwError){
+                throw new Error("es2015: We can't find the package.json file!");
             }
-            Fs.renameSync(_currentPath, _newPath);
-        });
-        
-        return true;
-    },
-    
-    getAllFromPackageFile(pkg){
-        if(!(pkg && pkg.es6Dependencies)){
-            return {}; // No packages found in the package.json file.
+            return false;
         }
-        return pkg.es6Dependencies;
+        
+        // Convert the package.json to an Object.
+        try{
+            json = JSON.parse(file);
+        }catch(error){
+            if(throwError){
+                throw new Error("es2015: Your package.json file is not valid json!");
+            }
+            return false;
+        }
+        
+        return json;
     },
     
-    getAllFromNodeModules(fromPackageFile){
+    /**
+     * Get all the packages that are es6.
+     * @param  {Object} fromPackageFile - A list with all the packages mentioned in the projects package.json file (uses es6Dependencies)
+     * @return {Array} All the packages that meet the requirements for an es6 package.
+     */
+    getAllPackages(fromPackageFile){
         let _modules = this.getAllNodeModules();
         
         // Check all the package files.
@@ -110,6 +90,10 @@ let ModuleLoader = {
         return _paths;
     },
     
+    /**
+     * Get the names of all the node_modules directories.
+     * @return {Array} All the node_modules names.
+     */
     getAllNodeModules(){
         if(!Fs.existsSync(this.paths.nodeModules)){
             return [];
@@ -123,6 +107,11 @@ let ModuleLoader = {
         return _modules;
     },
     
+    /**
+     * Check if the package is es6.
+     * @param  {String} module - The name of the module.
+     * @return {Boolean | Object} Object - the module/package name and the main path from the pacakge.json file.
+     */
     checkModuleForEs(module){
         let _path = Path.join(this.paths.nodeModules, module);
         let _pkg = this.getPackage(_path);
@@ -154,32 +143,89 @@ let ModuleLoader = {
         return {[module]:_mainPath};
     },
     
-    getPackage(path, throwError){
-        let file, json;
-        
-        path = Path.join(path, 'package.json');
-        
-        // Get the package.json file.
-        try{
-            file = Fs.readFileSync(path, 'utf-8');
-        }catch(error){
-            if(throwError){
-                throw new Error("es2015: We can't find the package.json file!");
-            }
-            return false;
+    /**
+     * Move all the packages from node_modules to es6_modules.
+     * @param  {Array} packages - All the package names.
+     */
+    movePackages(packages){
+        if(!Fs.existsSync(this.paths.es6Modules)){
+            Fs.mkdir(this.paths.es6Modules);
         }
         
-        // Convert the package.json to an Object.
-        try{
-            json = JSON.parse(file);
-        }catch(error){
-            if(throwError){
-                throw new Error("es2015: Your package.json file is not valid json!");
+        packages.forEach((_packageObject) => {
+            let _package = Object.keys(_packageObject)[0];
+            let _currentPath = Path.join(this.paths.nodeModules, _package);
+            let _newPath = Path.join(this.paths.es6Modules, _package);
+            
+            if(!Fs.existsSync(_currentPath)){
+                return false; // The module is already moved.
             }
-            return false;
+            Fs.renameSync(_currentPath, _newPath);
+        });
+    },
+    
+    /**
+     * Create a redirect from node_modules to es6_modules for all the es6 packages.
+     * @param  {Array} packages - All the package names and main paths.
+     */
+    createRedirectForPackages(packages){
+        packages.forEach((_packageObject) => {
+            let _package = Object.keys(_packageObject)[0];
+            let _mainPath = _packageObject[_package];
+            let _oldPath = Path.join(this.paths.nodeModules, _package);
+            let _currentPath = Path.join(this.paths.es6Modules, _package);
+            
+            // Generate a package.json and a index.js file.
+            let _indexFile = this.generateIndexFile(_package, _mainPath);
+            let _packageFile = this.generatePackageFile(_package);
+            
+            // Create the directory.
+            Fs.mkdirSync(_oldPath);
+            
+            // Create the package.json file.
+            Fs.writeFileSync(_oldPath + '/package.json', _packageFile);
+            
+            // Create the redirect index.js file.
+            Fs.writeFileSync(_oldPath + '/index.js', _indexFile);
+        });
+    },
+    
+    /**
+     * Generate the redirect index.js file.
+     * @param  {String} module   - The name of the module.
+     * @param  {String} mainPath - The path to the execute/main file.
+     * @return {String} The redirect index.js contents.
+     */
+    generateIndexFile(pkg, mainPath){
+        let _path = Path.join('../../../', pkg, mainPath);
+        let _file = "module.exports = require('"+_path+"');";//"import Module from '"+_path+"'; export default module;";
+        return _file;
+    },
+    
+    /**
+     * Generate the package.json file.
+     * @param  {String} pkg - The name of the module.
+     * @return {String} The contents of the new package.json.
+     */
+    generatePackageFile(pkg){
+        let _object = {
+            name: pkg,
+            main: 'index.js',
+            es6Replaced: true,
+        };
+        return JSON.stringify(_object);
+    },
+    
+    /**
+     * Get all the modules from the project/main package.json file.
+     * @param  {Object} pkg - A package.json file.
+     * @return {Object} All the es6Dependencies.
+     */
+    getAllFromPackageFile(pkg){
+        if(!(pkg && pkg.es6Dependencies)){
+            return {}; // No packages found in the package.json file.
         }
-        
-        return json;
+        return pkg.es6Dependencies;
     }
     
 };
